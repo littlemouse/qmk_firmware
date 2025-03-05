16a44d0f23455a48490038b60af983a5a92ac4db
Susie Lin <susie@littlemousetech.com>
2025-03-05 02:06:40 -0500

    adding rgb lighting adjustments and tweaked shift key

--- keyboards/zsa/planck_ez/keymaps/littlemouse/keymap.c
+++ keyboards/zsa/planck_ez/keymaps/littlemouse/keymap.c
@@ -31,10 +31,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_grid(
-    KC_TAB,         KC_Q,      KC_W,       KC_E,   KC_R,        KC_T,         KC_Y,       KC_U,        KC_I,    KC_O,    KC_P,           KC_BSPC,
-    KC_ESC,         KC_A,      KC_S,       KC_D,   KC_F,        KC_G,         KC_H,       RGUI_T(KC_J),KC_K,    KC_L,    RCTL_T(KC_SCLN),RSFT_T(KC_ENT),
-    KC_LSFT,        KC_Z,      KC_X,       KC_C,   KC_V,        KC_B,         KC_N,       KC_M,        KC_COMM, KC_DOT,  KC_SLSH,        KC_QUOT,
-    KC_RGUI,        KC_LCTL,   KC_LALT,    KC_LGUI,LOWER,       LT(4,KC_SPC), LT(4,KC_SPC),RAISE,      KC_LEFT, KC_DOWN, KC_UP,          KC_RGHT
+    KC_TAB,         KC_Q,      KC_W,       KC_E,   KC_R,   KC_T,     KC_Y,    KC_U,        KC_I,    KC_O,    KC_P,           KC_BSPC,
+    KC_ESC,         KC_A,      KC_S,       KC_D,   KC_F,   KC_G,     KC_H,    RGUI_T(KC_J),KC_K,    KC_L,    RCTL_T(KC_SCLN),KC_ENT,
+    KC_LSFT,        KC_Z,      KC_X,       KC_C,   KC_V,   KC_B,     KC_N,    KC_M,        KC_COMM, KC_DOT,  KC_SLSH,        RSFT_T(KC_QUOT),
+    KC_RGUI,        KC_LCTL,   KC_LALT,    KC_LGUI,LOWER,  KC_SPC,   KC_SPC,  RAISE,       KC_LEFT, KC_DOWN, KC_UP,          KC_RGHT
 ),
 
 /* Lower
@@ -69,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT_planck_grid(
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
     KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
-    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_1,    KC_2,    KC_3,    KC_PGDN, _______,
+    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_GRV,  KC_TILD, KC_3,    KC_PGDN, _______,
     _______, _______, _______, _______, _______, KC_MUTE, KC_MUTE, KC_MPLY, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT
 ),
 
@@ -86,10 +86,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] = LAYOUT_planck_grid(
-    _______, QK_BOOT, _______, RGB_TOG,  RGB_MOD, RGB_RMOD, RGB_HUI, RGB_HUD, RGB_VAI, RGB_VAD, _______, _______,
-    _______, _______, _______, LED_LEVEL, _______,  _______, _______, _______,  _______, _______, _______,_______,
-    _______, _______, _______,  _______, _______, _______, _______,  _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+    QK_BOOT, _______, LED_LEVEL, RGB_TOG,  _______, _______, _______, _______,  _______,  KC_BRMD, KC_BRMU, _______,
+    _______, _______, RGB_MOD,   RGB_RMOD, RGB_M_P, RGB_M_B, RGB_M_R, RGB_M_SW, RGB_M_SN, RGB_M_K, RGB_M_X, RGB_M_G,
+    _______, _______, RGB_HUI,   RGB_HUD,  _______, _______, _______, _______,  _______,  _______, _______, _______,
+    _______, _______, RGB_VAI,   RGB_VAD,  _______, _______, _______, _______,  _______,  _______, _______, _______
 ),
 
 [_NUMPAD] = LAYOUT_planck_grid(
